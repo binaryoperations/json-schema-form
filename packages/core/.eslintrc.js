b68b8be6180e6c5e1e8df7d24a9f26f9e350462b
Shashank Agarwal <shashank1010@gmail.com>
2024-09-06 14:39:56 +0300

    feat: add json schema parser and testers

---
+++ packages/core/.eslintrc.js
@@ -0,0 +1,48 @@
+/* eslint-env node */
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  // There is no file include in ESLint. Thus, ignore all and include files via negative ignore (!)
+  ignorePatterns: ["!/src", "!/test"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    "plugin:prettier/recommended",
+  ],
+  rules: {
+    semi: ["error", "always"],
+    // Allow extending interfaces without additional properties to create marker interfaces
+    "@typescript-eslint/no-empty-interface": [
+      "error",
+      {
+        allowSingleExtends: true,
+      },
+    ],
+    "@typescript-eslint/no-explicit-any": "off",
+    // Base rule must be disabled to avoid incorrect errors
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        argsIgnorePattern: "^_",
+        varsIgnorePattern: "^_",
+        caughtErrorsIgnorePattern: "^_",
+      },
+    ],
+    "import/no-unresolved": [
+      "error",
+      {
+        // Ignore ava import because it is incorrectly reported as unresolved despite working as expected.
+        ignore: ["^ava$"],
+      },
+    ],
+    "import/no-cycle": [
+      "error",
+      {
+        ignoreExternal: true,
+      },
+    ],
+  },
+};
---
+++ packages/core/schema/generate.ts
@@ -0,0 +1,251 @@
+import type {
+  ArrayJsonSchema,
+  BaseKeys,
+  BooleanJsonSchema,
+  JsonSchema,
+  NullJsonSchema,
+  NumberJsonSchema,
+  ObjectJsonSchema,
+  StringJsonSchema,
+} from "../models/JsonSchema";
+
+import uniqBy from "lodash/uniqBy";
+
+type Properties = { [property: string]: JsonSchema };
+
+export enum CONSTANTS {
+  ADDITIONAL_PROPS,
+  REQUIRED,
+  ARRAY,
+  STRING,
+  NUMBER,
+  NULL,
+}
+
+type ExpectedResponse = {
+  [CONSTANTS.REQUIRED]: string[];
+  [CONSTANTS.ADDITIONAL_PROPS]: Partial<
+    Omit<ObjectJsonSchema, BaseKeys | "required">
+  >;
+  [CONSTANTS.ARRAY]: Partial<Omit<ArrayJsonSchema, BaseKeys | "items">>;
+  [CONSTANTS.STRING]: Partial<Omit<StringJsonSchema, BaseKeys>>;
+  [CONSTANTS.NUMBER]: Partial<Omit<StringJsonSchema, BaseKeys>>;
+  [CONSTANTS.NULL]: Partial<Omit<StringJsonSchema, BaseKeys>>;
+};
+
+export class Generate<T extends JsonSchema = JsonSchema> {
+  static StringSchema: typeof Generate<StringJsonSchema>;
+  static NumberSchema: typeof Generate<NumberJsonSchema>;
+  static BooleanSchema: typeof Generate<BooleanJsonSchema>;
+  static NullSchema: typeof Generate<NullJsonSchema>;
+  static ArraySchema: typeof Generate<ArrayJsonSchema>;
+  static ObjectSchema: typeof Generate<ObjectJsonSchema>;
+
+  constructor(
+    protected deriveOptions: (
+      value: Properties | any,
+      path: string[]
+    ) => <T extends CONSTANTS>(prop: T) => ExpectedResponse[T]
+  ) {}
+
+  protected get Constructor(): typeof Generate {
+    return this.constructor as typeof Generate;
+  }
+
+  protected getTypedClass(type: string) {
+    switch (type) {
+      case "string":
+        return this.Constructor.StringSchema;
+      case "number":
+        return this.Constructor.NumberSchema;
+      case "boolean":
+        return this.Constructor.BooleanSchema;
+      case "object":
+        return this.Constructor.ObjectSchema;
+      case "array":
+        return this.Constructor.ArraySchema;
+      case "null":
+        return this.Constructor.NullSchema;
+    }
+
+    throw new Error(`Unhandled type: "${type}"`);
+  }
+
+  protected getTypedInstance(type: string) {
+    // TODO: infer type correctly
+    const Class = this.getTypedClass(type);
+    return new Class(this.deriveOptions);
+  }
+
+  protected preparePath(tail: string, head = "#/properties") {
+    return {
+      $id: [head, tail].join("/"),
+      path: [head.replace("#/properties", ""), tail],
+    };
+  }
+
+  protected parseProperties(object: object, head?: string) {
+    return Object.entries(object).reduce(
+      (properties: Properties | any, [key, value]) =>
+        Object.defineProperty(properties, key, {
+          value: this.parseValue(value, key, head),
+        }),
+      {}
+    );
+  }
+
+  protected parseArrayOrObject(
+    value: object | any[],
+    key: string,
+    head?: string
+  ) {
+    if (Array.isArray(value)) {
+      return this.getTypedInstance("array").prepareSchema(value, key, head);
+    }
+
+    return this.getTypedInstance("object").prepareSchema(value, key, head);
+  }
+
+  protected parseValue(value: any, key: string, head?: string): JsonSchema {
+    if (value === null)
+      return this.getTypedInstance("null").prepareSchema(value, "");
+
+    switch (typeof value) {
+      case "object":
+        return this.parseArrayOrObject(value, key, head);
+      default:
+        return this.getTypedInstance(typeof value).prepareSchema(
+          value,
+          key,
+          head
+        );
+    }
+  }
+
+  prepareSchema(value: any, _key: string, _head?: string): T {
+    throw new Error(
+      `"${
+        value === null ? "null" : typeof value
+      }" prepareSchema method is not defined on this class`
+    );
+  }
+
+  parse(object: object) {
+    return this.getTypedInstance("object").prepareSchema(object, "");
+  }
+}
+
+class GenerateObjectSchema extends Generate<ObjectJsonSchema> {
+  prepareSchema(value: object, key: string, head?: string): ObjectJsonSchema {
+    const { $id, path } = this.preparePath(key, head);
+    const properties = this.parseProperties(value, $id);
+    const deriveOptions = this.deriveOptions(properties, path);
+
+    const required = deriveOptions(CONSTANTS.REQUIRED);
+
+    const objectSchema: ObjectJsonSchema = {
+      $id,
+      type: "object",
+      properties,
+      ...deriveOptions(CONSTANTS.ADDITIONAL_PROPS),
+    };
+
+    if (required.length) {
+      objectSchema.required = required;
+    }
+
+    return objectSchema;
+  }
+}
+
+class GenerateArraySchema extends Generate<ArrayJsonSchema> {
+  private parseArray(value: any[], head: string) {
+    if (!value.length) return value;
+
+    const childrenSchema = value.map((item, i) =>
+      this.parseValue(item, `${i}`, head)
+    );
+
+    const distinctChildren = uniqBy(childrenSchema, "type");
+
+    if (distinctChildren.length === 1) return distinctChildren;
+
+    return {
+      oneOf: distinctChildren,
+    };
+  }
+
+  prepareSchema(value: any[], key: string, head?: string): ArrayJsonSchema {
+    const { $id, path } = this.preparePath(key, head);
+    const items = this.parseArray(value, $id);
+
+    const deriveOptions = this.deriveOptions(value, path);
+
+    return {
+      $id,
+      type: "array",
+      items: items,
+      ...deriveOptions(CONSTANTS.ARRAY),
+    };
+  }
+}
+
+class GenerateStringSchema extends Generate<StringJsonSchema> {
+  prepareSchema(value: string, key: string, head?: string): StringJsonSchema {
+    const { $id, path } = this.preparePath(key, head);
+
+    const deriveOptions = this.deriveOptions(value, path);
+
+    return {
+      $id,
+      type: "string",
+      ...deriveOptions(CONSTANTS.STRING),
+    };
+  }
+}
+class GenerateNumberSchema extends Generate<NumberJsonSchema> {
+  prepareSchema(value: number, key: string, head?: string): NumberJsonSchema {
+    const { $id, path } = this.preparePath(key, head);
+
+    const deriveOptions = this.deriveOptions(value, path);
+
+    return {
+      $id,
+      type: "number",
+      ...deriveOptions(CONSTANTS.NUMBER),
+    };
+  }
+}
+class GenerateBooleanSchema extends Generate<BooleanJsonSchema> {
+  prepareSchema(
+    _value: boolean,
+    key: string,
+    head?: string
+  ): BooleanJsonSchema {
+    const { $id } = this.preparePath(key, head);
+
+    return {
+      $id,
+      type: "boolean",
+    };
+  }
+}
+class GenerateNullSchema extends Generate<NullJsonSchema> {
+  prepareSchema(value: any[], key: string, head?: string): NullJsonSchema {
+    const { $id, path } = this.preparePath(key, head);
+    const deriveOptions = this.deriveOptions(value, path);
+
+    return {
+      $id,
+      type: "null",
+      ...deriveOptions(CONSTANTS.NULL),
+    };
+  }
+}
+
+Generate.StringSchema = GenerateStringSchema;
+Generate.NumberSchema = GenerateNumberSchema;
+Generate.BooleanSchema = GenerateBooleanSchema;
+Generate.ObjectSchema = GenerateObjectSchema;
+Generate.ArraySchema = GenerateArraySchema;
+Generate.NullSchema = GenerateNullSchema;
