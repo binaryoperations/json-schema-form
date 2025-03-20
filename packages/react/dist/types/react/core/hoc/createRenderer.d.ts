ced1478a1c25bf63921508be4fd54efa57c9f7ef
Shashank Agarwal <shashank1010@gmail.com>
2025-03-21 01:51:45 +0300

    build

--- packages/react/dist/types/react/core/hoc/createRenderer.d.ts
+++ packages/react/dist/types/react/core/hoc/createRenderer.d.ts
@@ -4,5 +4,6 @@ export declare const createLayoutRenderer: <P extends object>(Component: Compone
 } & P>;
 export declare const createCustomLayoutRenderer: <P extends object>(Component: ComponentType<PropsWithChildren>) => ComponentType<{
     id: string;
+    children?: PropsWithChildren['children'];
 } & P>;
 //# sourceMappingURL=createRenderer.d.ts.map
