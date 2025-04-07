c87c0fe656ce9537442dbca4a51dca3405b7e908
Shashank Agarwal <shashank1010@gmail.com>
2025-04-07 23:44:16 +0300

    better bundling

--- packages/playground/react/vite.config.ts
+++ packages/playground/react/vite.config.ts
@@ -1,5 +1,5 @@
 import react from '@vitejs/plugin-react';
-import path from 'node:path';
+// import path from 'node:path';
 import { defineConfig, type PluginOption } from 'vite';
 
 
@@ -7,10 +7,10 @@ import { defineConfig, type PluginOption } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()].flat() as PluginOption[],
-  resolve: {
-    alias: {
-      "@binaryoperations/json-forms-core": path.resolve(__dirname, "../../core"),
-      "@binaryoperations/json-forms-react": path.resolve(__dirname, "../../react"),
-    }
-  }
+  // resolve: {
+  //   alias: {
+  //     "@binaryoperations/json-forms-core": path.resolve(__dirname, "../../core"),
+  //     "@binaryoperations/json-forms-react": path.resolve(__dirname, "../../react"),
+  //   }
+  // }
 });
