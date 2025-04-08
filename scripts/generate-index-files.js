42207bf242046adbe73da5adfef45b876a9fc9b8
Shashank Agarwal <shashank1010@gmail.com>
2025-04-08 10:29:20 +0300

    restructure build process

--- scripts/generate-index-files.js
+++ scripts/generate-index-files.js
@@ -2,6 +2,8 @@
 const fs = require('fs');
 const path = require('path');
 
+
+
 const createIndexFiles = (directory) => {
   fs.readdirSync(directory, { withFileTypes: true }).forEach(dirent => {
     const fullPath = path.join(directory, dirent.name);
@@ -30,5 +32,15 @@ const createIndexFiles = (directory) => {
   });
 };
 
+// Parse command-line arguments
+const args = process.argv.slice(2);
+const fileArgIndex = args.indexOf('--dir');
+const fileName = fileArgIndex !== -1 && args[fileArgIndex + 1] ? args[fileArgIndex + 1] : null;
+
+if (!fileName) {
+  console.error('Error: --dir argument is required.');
+  process.exit(1);
+}
+
 // Run for both core and react
-createIndexFiles(path.join(__dirname, '..', 'packages', 'release', 'dist'));
+createIndexFiles(path.resolve(fileName));
