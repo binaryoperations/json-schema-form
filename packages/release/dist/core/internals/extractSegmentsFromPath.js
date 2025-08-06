13b7f25454eeb84444d1c091b07341da397ea104
Shashank Agarwal <shashank1010@gmail.com>
2025-08-06 12:43:09 +0530

    build

--- packages/release/dist/core/internals/extractSegmentsFromPath.js
+++ packages/release/dist/core/internals/extractSegmentsFromPath.js
@@ -1,10 +1,27 @@
 import isNaN from './isNaN';
 export function extractSegmentsFromPath(path) {
-    return extractSchmeaSegmentsFromPath(path.replaceAll(/#|properties|items\/?/g, ''));
+    const segments = path
+        .replace(/^#\//, "/")
+        .split("/")
+        .filter((part, index) => index !== 0 || !!part);
+    // ODD number of segments means that the path doesn't have /properties or /items
+    if (!(segments.length % 2))
+        return extractSchmeaSegmentsFromPath(path);
+    // If the path doesn't contain /#properties or /#item, return as is
+    if (!path.match(/\/#item|\/#properties/))
+        return extractSchmeaSegmentsFromPath(path);
+    path = segments.reduce((next, part, index) => {
+        if (index % 2)
+            return `${next}/${part}`;
+        if (["#item", "#properties"].includes(part))
+            return next;
+        throw new Error(`Malformed path: ${path}. Error after ${next}. Expected '#properties' or '#item' but got '${part}'`);
+    }, "");
+    return extractSchmeaSegmentsFromPath(path);
 }
 export function extractSchmeaSegmentsFromPath(path) {
     return path
-        .replaceAll(/#\/?/g, '')
+        .replace(/^#\//, "/")
         .split('/')
         .reduce((nodes, node) => !node ? nodes : nodes.concat(isNaN(node) ? node : +node), []);
 }
